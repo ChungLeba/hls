6d45d884eb92b5a626031c4d253575088e29d024
Chungleba <lebachungtdh@gmail.com>
2023-07-23 17:29:48 +0700

    fix http

--- app/page.tsx
+++ app/page.tsx
@@ -4,24 +4,30 @@ import Image from 'next/image'
 import Script from 'next/script'
 import React from 'react';
 import ReactHlsPlayer from "@ducanh2912/react-hls-player";
-
+import Head from 'next/head';
 //getData()
 
 export default async function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div>
-        <h1>
-          Hi
-        </h1>
-        <ReactHlsPlayer
-          src="http://103.74.101.152:8000/live/a22ae7bbf825442ca0017fef01b77178/index.m3u8"
-          autoPlay={false}
-          controls={true}
-          width="100%"
-          height="auto"
-        />
-      </div>
-    </main>
+    <>
+      <Head>
+        <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests" />
+      </Head>
+      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <div>
+          <h1>
+            Hi
+          </h1>
+          <ReactHlsPlayer
+            src="http://103.74.101.152:8000/live/a22ae7bbf825442ca0017fef01b77178/index.m3u8"
+            autoPlay={false}
+            controls={true}
+            width="100%"
+            height="auto"
+          />
+        </div>
+      </main>
+    </>
+
   )
 }
